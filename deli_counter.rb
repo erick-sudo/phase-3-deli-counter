ed272c1bb6fd47fe53eb05c66be28e533640a668
Erick Ochieng <erickochieng766@gmail.com>
2023-02-13 07:19:09 +0300

    Passed Test

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def line(queue)
+  unless queue.empty?
+    puts "The line is currently:#{queue.map.with_index { |customer, index| " #{index+1}. #{customer}" }.join("")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(queue, customer)
+  queue.push(customer)
+  puts "Welcome, #{customer}. You are number #{queue.length} in line."
+end
+
+def now_serving(queue)
+  if(queue.empty?)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{queue.shift}."
+  end
+end
